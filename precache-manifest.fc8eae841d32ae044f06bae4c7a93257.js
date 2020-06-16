4abe4494aa05b2910bb311cff09c63135612233c
eli-alkorta <elialkor@gmail.com>
2020-06-16 18:26:58 +0200

    Updates

---
+++ precache-manifest.fc8eae841d32ae044f06bae4c7a93257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275c2d1dc1919bbefa702fedaa69e585",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "bf72a497ada07e0d0778",
+    "url": "/portfolio/static/css/main.a5cd343b.chunk.css"
+  },
+  {
+    "revision": "970a2b3abf781e10101b",
+    "url": "/portfolio/static/js/2.03797dcf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.03797dcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf72a497ada07e0d0778",
+    "url": "/portfolio/static/js/main.7dcd586e.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
